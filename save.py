426d2934760a09db6c962535ae4f2fd6cfc74d7d
jezzaayt <gjaytoun@gmail.com>
2024-09-07 00:17:15 +0100

    refactor: fixed the IDs from repeating when opening and closing the file.

--- save.py
+++ save.py
@@ -4,9 +4,12 @@ import GUII
 counter = 1
 def save_data(original,temp, units, date, time):
     global counter 
+    check_counter(counter)
 
+    c = check_counter(counter)
+    print(check_counter(counter))
     data = {
-        "ID":counter,
+        "ID":c,
         "Original Value": original,
         "Temperature": temp,
         "Units": units, 
@@ -24,12 +27,35 @@ def save_data(original,temp, units, date, time):
             
     else:
         existing_data = []
-    
+  
     existing_data.append(data)
     counter += 1
+
+    
 
     with open(filename, "w") as file:
         json.dump(existing_data, file, indent=4)
         print("Temperature data saved successfully.")
   
-    
+
+def check_counter(counter):
+    # check if counter is in json 
+    filename = "temperature_data.json"
+    if os.path.exists(filename):
+        with open(filename, "r") as file:
+            try:
+                existing_data = json.load(file)
+            except json.JSONDecodeError:
+                existing_data = []
+            
+    else:
+        existing_data = []
+    existing_ids = [item["ID"] for item in existing_data]
+    if counter in existing_ids:
+
+        print(existing_ids[-1])
+        print(max(existing_ids))
+        newCounter = int(counter)
+        newCounter = max(existing_ids) + 1
+        return newCounter
+    return False
